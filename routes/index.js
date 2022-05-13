a804c81e72ee1aef71afb2ce84734c4d9d93342f
Gordo <gordonhdo@gmail.com>
2022-05-13 03:18:30 -0700

    adding comments // modify response msg

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+// import express router
+const router = require("express").Router();
 
-router.use('/api', apiRoutes);
+// route directory
+const apiRoutes = require("./api");
 
+// tell router to get our routes from the routes directory
+router.use("/api", apiRoutes);
+
+// error response
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send(
+    "<h2>Sorry,</h2><h2>The route you have entered was incorrect.</h2><h2>Please try again</h2><h3>server is still running...</h3>"
+  );
 });
 
 module.exports = router;
